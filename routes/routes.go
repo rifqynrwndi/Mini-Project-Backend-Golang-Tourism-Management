30ae63406a70533a61b5e9ce5dd5e567d6477a36
Rifqy Nirwandi <nirwandyrifqy@students.unnes.ac.id>
2024-11-07 14:47:17 +0700

    add tourists route for CRUD with admin only access

--- routes/routes.go
+++ routes/routes.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"tourism-monitoring/controllers/auth"
 	"tourism-monitoring/controllers/tourists"
+	"tourism-monitoring/middleware"
 
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
@@ -15,16 +16,20 @@ type RouteController struct {
 }
 
 func (rc RouteController) InitRoute(e *echo.Echo) {
-
+	// Authentication routes
 	e.POST("/login", rc.AuthController.LoginController)
 	e.POST("/register", rc.AuthController.RegisterController)
 
+	// Protected routes with JWT
 	eJWT := e.Group("")
 	eJWT.Use(echojwt.JWT([]byte(os.Getenv("JWT_SECRET_KEY"))))
 
-	eUser := eJWT.Group("/users")
-	eUser.GET("", rc.AuthController.LoginController)
-	eUser.POST("", rc.AuthController.RegisterController)
-
-	e.GET("/tourists", rc.TouristsController.GetAllTourists)
+	// Admin-only routes for tourists
+	eAdmin := eJWT.Group("/tourists")
+	eAdmin.Use(middleware.AdminOnly)
+	eAdmin.GET("", rc.TouristsController.GetAllTourists)
+	eAdmin.GET("/:id", rc.TouristsController.GetTouristByID)
+	eAdmin.POST("", rc.TouristsController.InsertTourist)
+	eAdmin.PUT("/:id", rc.TouristsController.UpdateTourist)
+	eAdmin.DELETE("/:id", rc.TouristsController.DeleteTourist)
 }
