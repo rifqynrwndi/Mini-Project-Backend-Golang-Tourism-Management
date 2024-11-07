5d888aa4ec22d00a5679e124d08b37533b025d4a
Rifqy Nirwandi <nirwandyrifqy@students.unnes.ac.id>
2024-11-07 14:33:47 +0700

    add tourists repo for CRUD

--- repositories/tourists/tourists_repo_interface.go
+++ repositories/tourists/tourists_repo_interface.go
@@ -4,4 +4,8 @@ import "tourism-monitoring/entities"
 
 type TouristsRepoInterface interface {
 	GetAllTourists() ([]entities.User, error)
+	GetTouristByID(id int) (entities.User, error)
+	InsertTourist(user entities.User) (entities.User, error)
+	UpdateTourist(id int, user entities.User) (entities.User, error)
+	DeleteTourist(id int) error
 }
